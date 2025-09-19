3a50287a52444271a86a4ddd04e7f492dc0260be
Samuel Badejo <samuelbadejo928@gmail.com>
2025-09-18 23:41:41 -0400

    fix(migrations): ensure exits.from_room_id is added before any index; neutralize early index migrations

--- priv/repo/migrations/20250916033609_add_indexes_to_rooms_and_exits.exs
+++ priv/repo/migrations/20250916033609_add_indexes_to_rooms_and_exits.exs
@@ -1,12 +1,4 @@
-defmodule Shard.Repo.Migrations.AddIndexesToRoomsAndExits do
+defmodule Shard.Repo.Migrations.ObsoleteFromRoomIdIndex do
   use Ecto.Migration
-
-  def change do
-    # If the unique index already exists, this will no-op
-    create_if_not_exists index(:rooms, [:slug], unique: true, name: :rooms_slug_index)
-
-    # Safe either way
-    create_if_not_exists index(:exits, [:from_room_id])
-    create_if_not_exists index(:exits, [:to_room_id])
-  end
+  def change, do: :ok
 end
