13877f9931306c7ae2ff15cee583fb8348e9249d
Nat Tuck <nat@ferrus.net>
2025-09-25 18:24:38 -0400

    more deploy instructions (#44)

--- notes/deploy/prod-env.sh
+++ notes/deploy/prod-env.sh
@@ -2,5 +2,5 @@ export MIX_ENV=prod
 export PHX_SERVER=true
 export PHX_HOST=shard.homework.quest
 export PORT=4242
-export DATABASE_URL=ecto://shard:[pwgen 14 1]@localhost/shard_prod
-export SECRET_KEY_BASE=[mix phx.gen.secret]
+export DATABASE_URL=ecto://shard:[password goes here]@localhost/shard_prod
+export SECRET_KEY_BASE=[secret key base goes here]
