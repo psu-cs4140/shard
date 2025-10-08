504c1019565ea85e8579c3e8119653623ec34dd6
Hunter H. <hunter-harris1@hotmail.com>
2025-10-08 15:10:23 -0400

    Update test_helper.exs

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,2 @@
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(Shard.Repo, :manual)
-
-# Load test fixtures
-Code.require_file("test/support/fixtures/characters_fixtures.ex")
-Code.require_file("test/support/fixtures/users_fixtures.ex")
