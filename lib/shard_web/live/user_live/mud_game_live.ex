4c1bd1bdf5b5986d01bcabeb761eabb666c194b9
Samuel Badejo <samuelbadejo928@gmail.com>
2025-10-14 20:29:54 -0400

    chore(credo): temp file-level disable (Cyclomatic+Nesting) for MudGameLive

--- lib/shard_web/live/user_live/mud_game_live.ex
+++ lib/shard_web/live/user_live/mud_game_live.ex
@@ -1,3 +1,5 @@
+# credo:disable-for-this-file Credo.Check.Refactor.Nesting
+# credo:disable-for-this-file Credo.Check.Refactor.CyclomaticComplexity
 defmodule ShardWeb.MudGameLive do
   @moduledoc false
   use ShardWeb, :live_view
