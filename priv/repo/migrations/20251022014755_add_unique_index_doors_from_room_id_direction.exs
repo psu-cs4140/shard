ef30c09c579a2ddb120f3064f856a6f8c8209912
Rider Mccallum <ridermccallum@gmail.com>
2025-10-21 23:49:33 -0400

    added unique index to migration

--- priv/repo/migrations/20251022014755_add_unique_index_doors_from_room_id_direction.exs
+++ priv/repo/migrations/20251022014755_add_unique_index_doors_from_room_id_direction.exs
@@ -2,5 +2,6 @@ defmodule Shard.Repo.Migrations.AddUniqueIndexDoorsFromRoomIdDirection do
   use Ecto.Migration
 
   def change do
+    create unique_index(:doors, [:from_room_id, :direction])
   end
 end
