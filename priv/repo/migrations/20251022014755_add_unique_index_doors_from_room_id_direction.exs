37aef65fe837de622d1cf2cfbab4b54277fc8291
Rider Mccallum <ridermccallum@gmail.com>
2025-10-21 23:58:26 -0400

    one more try

--- priv/repo/migrations/20251022014755_add_unique_index_doors_from_room_id_direction.exs
+++ priv/repo/migrations/20251022014755_add_unique_index_doors_from_room_id_direction.exs
@@ -2,6 +2,8 @@ defmodule Shard.Repo.Migrations.AddUniqueIndexDoorsFromRoomIdDirection do
   use Ecto.Migration
 
   def change do
-    create unique_index(:doors, [:from_room_id, :direction])
+    unless index_exists?(:doors, [:from_room_id, :direction]) do
+      create unique_index(:doors, [:from_room_id, :direction])
+    end
   end
 end
