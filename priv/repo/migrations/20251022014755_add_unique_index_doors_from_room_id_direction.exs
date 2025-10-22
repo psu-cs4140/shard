a40772e6ce0bbe76e4e8bc3cccceb4b3dd375ebc
Rider Mccallum <ridermccallum@gmail.com>
2025-10-22 09:21:02 -0400

    migration issues

--- priv/repo/migrations/20251022014755_add_unique_index_doors_from_room_id_direction.exs
+++ priv/repo/migrations/20251022014755_add_unique_index_doors_from_room_id_direction.exs
@@ -6,4 +6,21 @@ defmodule Shard.Repo.Migrations.AddUniqueIndexDoorsFromRoomIdDirection do
       create unique_index(:doors, [:from_room_id, :direction])
     end
   end
+
+  defp index_exists?(table_name, columns) do
+    # Query to check if the index exists
+    query = """
+    SELECT 1 FROM pg_indexes
+    WHERE tablename = $1
+    AND indexdef LIKE '%' || $2 || '%'
+    """
+
+    case Ecto.Adapters.SQL.query(Shard.Repo, query, [
+           Atom.to_string(table_name),
+           Enum.join(columns, "_")
+         ]) do
+      {:ok, %{rows: [[_]]}} -> true
+      _ -> false
+    end
+  end
 end
