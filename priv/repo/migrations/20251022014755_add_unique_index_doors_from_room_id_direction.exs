41f85076992505e7e922483878b41e4fa5636e85
Rider Mccallum <ridermccallum@gmail.com>
2025-10-23 19:14:35 -0400

    ..

--- priv/repo/migrations/20251022014755_add_unique_index_doors_from_room_id_direction.exs
+++ priv/repo/migrations/20251022014755_add_unique_index_doors_from_room_id_direction.exs
@@ -2,25 +2,6 @@ defmodule Shard.Repo.Migrations.AddUniqueIndexDoorsFromRoomIdDirection do
   use Ecto.Migration
 
   def change do
-    unless index_exists?(:doors, [:from_room_id, :direction]) do
-      create unique_index(:doors, [:from_room_id, :direction])
-    end
-  end
-
-  defp index_exists?(table_name, columns) do
-    # Query to check if the index exists
-    query = """
-    SELECT 1 FROM pg_indexes
-    WHERE tablename = $1
-    AND indexdef LIKE '%' || $2 || '%'
-    """
-
-    case Ecto.Adapters.SQL.query(Shard.Repo, query, [
-           Atom.to_string(table_name),
-           Enum.join(columns, "_")
-         ]) do
-      {:ok, %{rows: [[_]]}} -> true
-      _ -> false
-    end
+    execute "CREATE UNIQUE INDEX IF NOT EXISTS doors_from_room_id_direction_idx ON doors (from_room_id, direction)"
   end
 end
