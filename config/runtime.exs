25ace420f71f603c6cc353b856fc185d0639f44e
Shard App <shard@ettin.fogcloud.org>
2025-10-30 16:05:38 -0400

    works with mailjet

--- config/runtime.exs
+++ config/runtime.exs
@@ -70,56 +70,6 @@ if config_env() == :prod do
       port: port
     ],
     secret_key_base: secret_key_base
-
-  # ## SSL Support
-  #
-  # To get SSL working, you will need to add the `https` key
-  # to your endpoint configuration:
-  #
-  #     config :shard, ShardWeb.Endpoint,
-  #       https: [
-  #         ...,
-  #         port: 443,
-  #         cipher_suite: :strong,
-  #         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-  #         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
-  #       ]
-  #
-  # The `cipher_suite` is set to `:strong` to support only the
-  # latest and more secure SSL ciphers. This means old browsers
-  # and clients may not be supported. You can set it to
-  # `:compatible` for wider support.
-  #
-  # `:keyfile` and `:certfile` expect an absolute path to the key
-  # and cert in disk or a relative path inside priv, for example
-  # "priv/ssl/server.key". For all supported SSL configuration
-  # options, see https://hexdocs.pm/plug/Plug.SSL.html#configure/1
-  #
-  # We also recommend setting `force_ssl` in your config/prod.exs,
-  # ensuring no data is ever sent via http, always redirecting to https:
-  #
-  #     config :shard, ShardWeb.Endpoint,
-  #       force_ssl: [hsts: true]
-  #
-  # Check `Plug.SSL` for all available options in `force_ssl`.
-
-  # ## Configuring the mailer
-  #
-  # In production you need to configure the mailer to use a different adapter.
-  # Here is an example configuration for Mailgun:
-  #
-  #     config :shard, Shard.Mailer,
-  #       adapter: Swoosh.Adapters.Mailgun,
-  #       api_key: System.get_env("MAILGUN_API_KEY"),
-  #       domain: System.get_env("MAILGUN_DOMAIN")
-  #
-  # Most non-SMTP adapters require an API client. Swoosh supports Req, Hackney,
-  # and Finch out-of-the-box. This configuration is typically done at
-  # compile-time in your config/prod.exs:
-  #
-  #     config :swoosh, :api_client, Swoosh.ApiClient.Req
-  #
-  # See https://hexdocs.pm/swoosh/Swoosh.html#module-installation for details.
 end
 
 ## ── Mailer (env-driven) ────────────────────────────────────────────────────────
@@ -143,17 +93,39 @@ end
 #  auth: :always,
 #  retries: 2
 
-config :shard, Shard.Mailer,
-  adapter: Swoosh.Adapters.SMTP,
-  # SMTP settings (ignored when adapter is Local)
-  relay: "localhost",
-  port: 25,
-  tls: :never,
-  auth: :never,
-  retries: 2
+### SMTP version
 
-
-
+#config :shard, Shard.Mailer,
+#  adapter: Swoosh.Adapters.SMTP,
+#  # SMTP settings (ignored when adapter is Local)
+#  relay: "localhost",
+#  port: 25,
+#  tls: :never,
+#  auth: :never,
+#  retries: 2
 
 # Swoosh API client not needed for SMTP/Local
-config :swoosh, :api_client, false
+# config :swoosh, :api_client, false
+
+### Mailjet Version
+
+mailjet_key =
+  System.get_env("MAILJET_KEY") ||
+    raise """
+    environment variable MAILJET_KEY is missing.
+    Gonna need that for this to work.
+    """
+
+[mjet_key, mjet_sec] =
+  mailjet_key
+  |> String.trim()
+  |> String.split(":")
+
+config :shard, Shard.Mailer,
+  adapter: Swoosh.Adapters.Mailjet,
+  api_key: mjet_key,
+  secret: mjet_sec,
+  send_from: {"Shard", "no-reply@homework.quest"}
+  
+config :swoosh, :api_client, Swoosh.ApiClient.Req 
+
