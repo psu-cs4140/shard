da17ddfc8e4ed5568fd055f5a096229ac3f29970
Your Name <you@example.com>
2025-11-21 19:01:39 -0500

    format

--- priv/repo/migrations/20250925000001_create_items.exs
+++ priv/repo/migrations/20250925000001_create_items.exs
@@ -4,7 +4,7 @@ defmodule Shard.Repo.Migrations.CreateItems do
   def change do
     # This migration is now a no-op since the items table is created in the earlier migration
     # 20241121000001_add_item_stats_support.exs with all the necessary fields and indexes
-    
+
     # If the table doesn't exist for some reason, we could add a fallback here,
     # but normally the earlier migration should handle everything
     :ok
