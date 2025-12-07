17b816e4a796e75a0f7f46383a5ed4df0cc3f108
longshankz <tylersmallidge@gmail.com>
2025-12-07 03:10:45 +0000

    feat: complete item durability system implementation

--- priv/repo/migrations/003_add_current_durability_to_character_equipment.exs
+++
@@ -1,9 +0,0 @@
-defmodule Shard.Repo.Migrations.AddCurrentDurabilityToCharacterEquipment do
-  use Ecto.Migration
-
-  def change do
-    alter table(:character_equipment) do
-      add :current_durability, :integer
-    end
-  end
-end
